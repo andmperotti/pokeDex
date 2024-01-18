6fdb8be191215a410e731b7768adcc0fded380bb
Andrew Perotti <Perotti28@gmail.com>
2024-01-18 10:58:26 -0800

    Added functionality for showing locatins where a pokemon can be found, they're displayed into a list which is hidden before this action. Now that I think of it I need to add logic that states that you can't get locations unless you search for a pokemon first.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('#search').addEventListener('click', getFetch)
+let locations;
 
 function getFetch(){
   const choice = document.querySelector('input').value.toLowerCase()
@@ -8,13 +9,36 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        // console.log(data)
         document.querySelector('h2').innerText = `Name: ${data.name}`
         document.querySelector('h3').innerText = `Type: ${data.types[0].type.name}`
         document.querySelector('#poke').src = data.sprites.front_default
         document.querySelector('#poke').alt = `An image of a ${data.name}`
+        locations= data.location_area_encounters
+        // console.log(locations)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+document.querySelector('#list-locations').addEventListener('click', getLocations)
+function getLocations(){
+  fetch(locations)
+    .then(res=>res.json())
+    .then(data=>{
+      // console.log(data)
+      document.querySelector('ul').removeAttribute('hidden')
+      let locationList =[]
+      for(let location of data){
+        locationList.push(location.location_area.name)
+      }
+      // console.log(locationList)
+      for(let specific of locationList){
+        let li = document.createElement('li')
+        li.appendChild(document.createTextNode(`${specific}`))
+        document.querySelector('ul').appendChild(li)
+      }
+    })
+    .catch(err=>console.log(`error: ${err}`))
 }
